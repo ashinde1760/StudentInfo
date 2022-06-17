9ba960304fdbaa97d47cb3c21709913be495a180
ashinde1760 <ashinde1760@gmail.com>
2022-06-18 02:32:33 +0530

    updated code

--- src/main/java/com/studentInfo/entity/Controller/StudentController.java
+++ src/main/java/com/studentInfo/entity/Controller/StudentController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.studentInfo.entity.Student;
 
 @RestController
+@CrossOrigin("*")
 public class StudentController {
 
 	@Autowired
